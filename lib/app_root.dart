f5d73210ec5418690220ee89859c61b70d6a486e
sayedmoataz <sayedmoataz9@gmail.com>
2024-11-27 21:15:50 +0200

    delete unnecessary packages

--- lib/app_root.dart
+++ lib/app_root.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-
-import 'features/form_screen/presentation/pages/form_screen.dart';
 import 'utils/app_colors.dart';
 import 'utils/app_routes.dart';
 import 'utils/app_strings.dart';
@@ -26,22 +22,6 @@ class MyAppRoot extends StatelessWidget {
             color: AppColors.primaryColor,
             initialRoute: '/',
             onGenerateRoute: AppRoutes.onGeneratedRoute,
-            home: OfflineBuilder(
-              connectivityBuilder: (
-                BuildContext context,
-                List<ConnectivityResult> connectivity,
-                Widget child,
-              ) {
-                final bool connected = connectivity != ConnectivityResult.none;
-                if (connected) {
-                  return const FormScreen();
-                } else {
-                  return const SizedBox.shrink();
-                }
-              },
-              child: Center(
-                  child: CircularProgressIndicator(color: AppColors.secondaryColor)),
-            ),
           );
         });
   }
